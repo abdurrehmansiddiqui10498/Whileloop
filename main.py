c7deddcb3bade3dabcef626eb3214f894a8d7e06
Abdur Rehman Siddiqui <abisbah2011@gmail.com>
2024-07-05 13:54:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# How to print any table
+a = int(input("enter a number"))
+b = 1
+while b <= 12:
+  c = a*b
+  print(a,"x",b,"=",c)
+  b += 1  
+
+# How to construct a Counter/Timer
+number = int(input("'Enter a number'"))
+while number > 0:
+  print(number)
+  number = number - 1
